55cda9622566a18e5ad2624c06b8474d0e25551b
Owen <owenyangg@gmail.com>
2016-05-11 18:46:24 -1000

    sets up to run via server.js file

---
+++ server.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const express = require('express'),
+      app     = express(),
+      PORT    = process.env.PORT || 3000
+      ;
+
+app.use(express.static('public'));
+
+app.listen(PORT, function(){
+  console.log(`Server listening on port: ${PORT}`);
+});
